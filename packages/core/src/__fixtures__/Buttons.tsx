e7a18ae55674b61698f3de88462fa5e01aba98f9
GabrielaDiazB <gabriela.dbravo@gmail.com>
2025-01-06 18:30:39 -0500

    feat: add button component examples

--- packages/core/src/__fixtures__/Buttons.tsx
+++ packages/core/src/__fixtures__/Buttons.tsx
@@ -1,18 +1,68 @@
-import { useState } from "react";
+import { useState, type FC } from "react";
 import { Button } from "@/components/ui/button";
+import { Calculator, ChevronRight, Loader2, Mouse } from "lucide-react";
+import { useFixtureSelect, useFixtureInput } from "react-cosmos/client";
 
-const ButtonFixture = () => {
-	const [clickCount, setClickCount] = useState(0);
+const useClickCount = (): { clickCount: number; handleClick: () => void } => {
+	const [clickCount, setClickCount] = useState<number>(0);
+	const handleClick = (): void => setClickCount(clickCount + 1);
+	return { clickCount, handleClick };
+};
 
-	const handleClick = () => {
-		setClickCount(clickCount + 1);
+// Common button
+const DefaultButton: FC = () => {
+	const [done, setDone] = useFixtureInput("Icon", false);
+	const { clickCount, handleClick } = useClickCount();
+
+	const [buttonType] = useFixtureSelect("Button Types", {
+		options: [
+			"default",
+			"secondary",
+			"outline",
+			"destructive",
+			"ghost",
+			"link",
+		],
+		defaultValue: "default",
+	});
+
+	const [selectedIcon] = useFixtureSelect("Icons", {
+		options: ["ChevronRight", "Mouse", "Calculator"],
+		defaultValue: "Mouse",
+	});
+
+	const iconMap: Record<string, React.ElementType> = {
+		Calculator,
+		ChevronRight,
+		Mouse,
 	};
+
+	const IconComponent = iconMap[selectedIcon];
 
 	return (
 		<div className="h-screen flex items-center justify-center bg-gray-100">
-			<Button onClick={handleClick}>{`Click count: ${clickCount}`}</Button>
+			<Button variant={buttonType} onClick={handleClick}>
+				{done && IconComponent ? <IconComponent /> : null}
+				{`Click count: ${clickCount}`}
+			</Button>
 		</div>
 	);
 };
 
-export default ButtonFixture;
+// Loading button
+const LoadingButton: FC = () => {
+	const { clickCount, handleClick } = useClickCount();
+	return (
+		<div className="h-screen flex items-center justify-center bg-gray-100">
+			<Button disabled>
+				<Loader2 className="animate-spin" />
+				Please wait
+			</Button>
+		</div>
+	);
+};
+
+export default {
+	examples: DefaultButton,
+	loading: LoadingButton,
+};
