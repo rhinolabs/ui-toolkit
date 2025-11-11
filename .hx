3837d49c46b1ab6c9133e9587c19f25d16ae7a1d
Christian Llontop <me@chrisllontop.com>
2025-11-10 23:29:16 -0500

    Update .hx

--- .hx
+++ .hx
@@ -1,9 +1,9 @@
 {
   "project_name": "Rhinolabs Site",
-  "project_id": "proj_6912b1650eee54939a99d10d",
+  "project_id": "proj_6912b91df3623b39bd1d795a",
   "project_alternate_id": "rhinolabs-site",
-  "app_name": "ui-toolkit",
-  "app_id": "app_6912b234f3623b39bd1d64b4",
+  "app_name": "UI Toolkit",
+  "app_id": "app_6912ba070eee54939a99e9b5",
   "app_alternate_id": "ui-toolkit",
   "organization_id": "org_67d2118fd2b3a74e483a786e"
 }
