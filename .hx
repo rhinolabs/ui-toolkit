aab18d3d96e978528d563f15e516db5f62253303
Christian Llontop <me@chrisllontop.com>
2025-11-10 19:49:11 -0500

    Add .hx configuration

--- .hx
+++ .hx
@@ -1,9 +1,9 @@
 {
-  "project_name": "Open Source",
-  "project_id": "proj_690302d1c960d41b0e564d55",
-  "project_alternate_id": "open-source",
+  "project_name": "Rhinolabs Site",
+  "project_id": "proj_69127e3d0eee54939a997609",
+  "project_alternate_id": "rhinolabs-site",
   "app_name": "UI Toolkit",
-  "app_id": "app_69030b9fc771ff6d6c6575de",
+  "app_id": "app_691287c4060e27ab5d576ce0",
   "app_alternate_id": "ui-toolkit",
   "organization_id": "org_67d2118fd2b3a74e483a786e"
 }
